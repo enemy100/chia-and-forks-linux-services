4e0561110b5fdedf957a7e59b27d1c6ce03ab863
enemy100 <robson@robsongomes.com>
2021-11-11 22:40:21 -0800

    chia services

---
+++ chia/chia-server-start.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+
+cd /usr/lib/chia-blockchain/resources/app.asar.unpacked/daemon
+
+./chia start farmer
+
+#sleep 5
+
+exit 
---
+++ chia/chia-server-stop.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/bash
+
+cd /usr/lib/chia-blockchain/resources/app.asar.unpacked/daemon
+
+./chia stop farmer
+
+exit 0
